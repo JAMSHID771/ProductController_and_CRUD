300f8835867326559bc8e9cc69465570bf24532c
Jamshid <asilbek090900@gmail.com>
2025-03-03 17:45:45 +0500

    Routes updated for ProductController with CRUD and search methods

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/create', [ProductController::class, 'create']);
+Route::get('/products/{id}', [ProductController::class, 'show']); 
+Route::get('/products/{id}/edit', [ProductController::class, 'edit']); 
+Route::delete('/products/{id}', [ProductController::class, 'destroy']); 
+Route::get('/products/search/{text}', [ProductController::class, 'search']);
